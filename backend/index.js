2a171fc74ef0234158f238dfd893f5fd27ba8019
abhinavVP <abhinav.pattanshetti@gmail.com>
2023-11-17 12:29:41 +0530

    index stuff

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRouter = require('./routes/auth');
-const Request = require('./models/request');
-
+const adminRouter = require('./routes/admin');
+const workerRouter = require('./routes/worker');
+const citizenRouter = require('./routes/citizen');
 const app = express();
 const PORT = 5000;
 
@@ -12,14 +13,18 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 
+app.use('/auth', authRouter);
+app.use('/admin', adminRouter);
+app.use('/citizen', citizenRouter);
+// app.use('/worker', workerRouter);
+
+
 const uri = "mongodb+srv://garbage:garbage@gms.qxawdmv.mongodb.net/?retryWrites=true&w=majority";
 
-
-app.use('/auth', authRouter);
-
 mongoose.connect(uri)
-.then(()=> console.log('akash lowda'));
-
+.then(()=>{
+    console.log('connected');
+})
 
 
 app.listen(PORT, ()=>{
