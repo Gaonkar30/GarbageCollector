4cc3afdf43b8bb7aefab3110d6450d61cd6a4353
Gaonkar30 <akashggaonkar@gmail.com>
2023-11-28 12:03:13 +0530

    team ui

--- frontend/src/Pages/TheTeampg.jsx
+++ frontend/src/Pages/TheTeampg.jsx
@@ -15,11 +15,11 @@ function TheTeampg() {
         <div class="row">
 
 
-           <TeamCard imgaddress={require("../components/boy.png")} teamName="Abinav Hubli" title="some guy" persondes="nigga" email="nigga12@gmail.com" twitterlink="#" facebooklink="#" LinkedinLink="#" InstagramLink="#" ></TeamCard>
+           <TeamCard imgaddress={require("../components/boy.png")} teamName="Abinav Hubli" title="some guy" persondes="nigga" email="abinavp@gmail.com" twitterlink="#" facebooklink="#" LinkedinLink="#" InstagramLink="#" ></TeamCard>
 
-           <TeamCard imgaddress={require("../components/boy.png")} teamName="Abinav Hubli" title="some guy" persondes="nigga" email="nigga12@gmail.com" twitterlink="#" facebooklink="#" LinkedinLink="#" InstagramLink="#" ></TeamCard>
+           <TeamCard imgaddress={require("../components/boy.png")} teamName="Akash G  Gaonkar" title="some guy" persondes="nigga" email="akash@gmail.com" twitterlink="#" facebooklink="#" LinkedinLink="#" InstagramLink="#" ></TeamCard>
 
-           <TeamCard imgaddress={require("../components/boy.png")} teamName="Abinav Hubli" title="some guy" persondes="nigga" email="nigga12@gmail.com" twitterlink="#" facebooklink="#" LinkedinLink="#" InstagramLink="#" ></TeamCard>
+           <TeamCard imgaddress={require("../components/boy.png")} teamName="Abishek Kundapur" title="some guy" persondes="nigga" email="abhishek@gmail.com" twitterlink="#" facebooklink="#" LinkedinLink="#" InstagramLink="#" ></TeamCard>
 
            
         </div>
