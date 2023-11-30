14e8d1a843c3162ecc64726c146545d1ad756716
abhinavVP <abhinav.pattanshetti@gmail.com>
2023-11-30 09:42:51 +0530

    aksh lowda

---
+++ backend/worker.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const workerSchema = new Schema({
+    name : String,
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    vehicleType : {
+        type : String,
+        required : true
+    },
+    currentTask : String
+});
+
+const Worker = mongoose.model('Worker', workerSchema);
+
+module.exports = Worker;
